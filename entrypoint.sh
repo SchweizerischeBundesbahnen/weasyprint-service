58c8198ba8bf2aea87a4db449789d79302d9f34a
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-09-19 14:10:10 +0200

    fix: enable hardware acceleration using env variable (#68)
    Refs: #62

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,9 @@ if ! pgrep -x 'dbus-daemon' > /dev/null; then
     if [ -f /run/dbus/pid ]; then
         rm /run/dbus/pid
     fi
-    dbus-daemon --system --fork;
+    dbus_session_bus_address_filename="/tmp/dbus_session_bus_address";
+    dbus-daemon --system --fork --print-address > ${dbus_session_bus_address_filename};
+    export DBUS_SESSION_BUS_ADDRESS=$(cat ${dbus_session_bus_address_filename})
 fi
 
 python app/WeasyprintServiceApplication.py &

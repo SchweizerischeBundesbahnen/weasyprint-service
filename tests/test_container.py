921aa247d3781a39c3458e7d4359a4cec3e9be76
u237713 <sebastian.scheidegger@sbb.ch>
2025-02-24 16:40:56 +0100

    fix: fix alpine dbus problems
    Change tag name and port of test_container

--- tests/test_container.py
+++ tests/test_container.py
@@ -6,8 +6,8 @@ import docker
 def test_container():
     client = docker.from_env()
 
-    image, _ = client.images.build(path=".", tag="wps", buildargs={"APP_IMAGE_VERSION": "1.0.0"})
-    container = client.containers.run(image=image, detach=True, name="wps", ports={"9081": 9081})
+    image, _ = client.images.build(path=".", tag="weasyprint_service", buildargs={"APP_IMAGE_VERSION": "1.0.0"})
+    container = client.containers.run(image=image, detach=True, name="weasyprint_service", ports={"9080": 9080})
     time.sleep(5)
     logs = container.logs()
     container.stop()

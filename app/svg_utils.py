e8a34e89999f94702736a4750594c4aa07e6402a
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-03-24 15:47:14 +0100

    fix: support vw/vh and percents in SVG dimensions

--- app/svg_utils.py
+++ app/svg_utils.py
@@ -181,7 +181,7 @@ def svg_to_string(svg: Element) -> str:
     Returns:
         str: SVG content as string.
     """
-    ET.register_namespace("", "http://www.w3.org/2000/svg")
+    ET.register_namespace("", "http://www.w3.org/2000/svg")  # pylint: disable=invalid-url
     return ET.tostring(svg, encoding="unicode")
 
 

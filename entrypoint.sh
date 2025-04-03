845a8f9848a1b03872129edba67c00184c5523a1
Adam Ruberti <adam.ruberti@sbb.ch>
2025-04-03 10:23:13 +0200

    chore: fix entrypoint.sh WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ CHROMIUM_VERSION="$(${CHROMIUM_EXECUTABLE_PATH} --version | awk '{print $2}')"
 export WEASYPRINT_SERVICE_CHROMIUM_VERSION=${CHROMIUM_VERSION}
 
 if ! pgrep -x 'dbus-daemon' > /dev/null; then
-    if [ -f /run/dbus/dbus.pid ]; then
-        rm /run/dbus/dbus.pid
+    if [ -f /run/dbus/pid ]; then
+        rm /run/dbus/pid
     fi
     dbus_session_bus_address_filename="/tmp/dbus_session_bus_address";
     dbus-daemon --system --fork --print-address > ${dbus_session_bus_address_filename};

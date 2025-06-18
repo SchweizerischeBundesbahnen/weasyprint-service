77c477d6ad725b6acfdaedca1597d242a34db306
ariwk <adam.ruberti@sbb.ch>
2025-06-18 11:32:47 +0200

    fix(tests): update WeasyPrint controller tests to use 'content' parameter

--- tests/test_weasyprint_controller.py
+++ tests/test_weasyprint_controller.py
@@ -26,7 +26,13 @@ def test_convert_html():
     os.environ["CHROMIUM_EXECUTABLE_PATH"] = test_script_path
     os.environ["WEASYPRINT_SERVICE_VERSION"] = "test1"
     with TestClient(app) as test_client:
-        result = test_client.post("/convert/html?base_url=/", json='<img src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMTAwcHgiPC9zdmc+"/>"')
+        result = test_client.post(
+            "/convert/html?base_url=/",
+            content='<img src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMTAwcHgiPC9zdmc+"/>',
+        )
         assert result.status_code == 200
-        result = test_client.post("/convert/html", data=b'\x81<img src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMTAwcHgiPC9zdmc+"/>"')
+        result = test_client.post(
+            "/convert/html",
+            content=b'\x81<img src="data:image/svg+xml;base64,PHN2ZyBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMTAwcHgiPC9zdmc+"/>',
+        )
         assert result.status_code == 400

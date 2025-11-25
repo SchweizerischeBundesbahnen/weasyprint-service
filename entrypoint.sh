41f7c8607423972a3763c6177406cd5dcb28a0d8
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-11-25 12:20:50 +0100

    fix: disable uvicorn sync mode in entrypoint script
    Refs: #267

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export WEASYPRINT_SERVICE_BUILD_TIMESTAMP=${BUILD_TIMESTAMP}
 # Update font cache to include any custom mounted fonts
 fc-cache -f
 
-uv run python -m app.weasyprint_service_application &
+uv run --no-sync python -m app.weasyprint_service_application &
 
 wait -n
 
